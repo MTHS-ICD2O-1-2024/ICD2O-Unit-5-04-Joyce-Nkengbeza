7e446b113db57d8f280f29ae85b3bedf94851f07
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-05-05 16:33:41 +0000

    day

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Joyce Nkengbeza
-// Created on: Mar 2025
+// Created on: April 2025
 // This file contains the JS functions for index.html
 
 'use strict'
@@ -9,11 +9,10 @@
 function calculate() {
   // input
   const age = document.getElementById("age").value;
-  const tuesday = document.getElementById("tuesday").checked;
-  const thursday = document.getElementById("thursday").checked;
+  const day = document.getElementById("day").checked;
 
   // process
-    if (tuesday || thursday || (age >= 12 && age <=21)) {
+    if ( day == 3 || day == 5 || (age >= 12 && age <=21)) {
     // output
     document.getElementById("results").innerHTML =
       "<p>You can recieve a student discount.</p>";
